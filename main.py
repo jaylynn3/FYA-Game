38367b9075092381bd9e687d61fc702b7d8db499
Jaylyn <jaylynn140707@gmail.com>
2026-06-05 19:19:03 -0400

    car

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("My First Game")
+pygame.display.set_caption("Game")
+
+#
+car_x = 150
+car_y = 150
+
+lanes = [120, 220, 320, 420]
+current_lane = 1
 
 running = True
 
@@ -12,7 +19,31 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 100, 255))
+    # car movement controls
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if current_lane > 0:
+                    current_lane -= 1
+
+            if event.key == pygame.K_DOWN:
+                if current_lane < len(lanes) - 1:
+                    current_lane += 1
+
+    car_y = lanes[current_lane]
+
+    #background color
+    screen.fill((157, 193, 183))
+
+    #the race track
+    pygame.draw.rect(screen, (100, 100, 100), (50, 50, 700, 500))
+    pygame.draw.line(screen, (255, 255, 255), (50, 175), (750, 175), 3)
+    pygame.draw.line(screen, (255, 255, 255), (50, 275), (750, 275), 3)
+    pygame.draw.line(screen, (255, 255, 255), (50, 375), (750, 375), 3)
+
+    #the car
+    pygame.draw.rect(screen, (255, 0, 0), (car_x, car_y, 50, 30))
+
+
     pygame.display.flip()
 
 pygame.quit()
